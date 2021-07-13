75f6b4ac1049cfff85c5871f44edf62a9db2985c
BitsAndDroids <77780263+BitsAndDroids@users.noreply.github.com>
2021-07-13 11:42:34 +0200

    push latest

--- outputs/outputbundle.h
+++ outputs/outputbundle.h
@@ -1,11 +1,21 @@
 #ifndef OUTPUTBUNDLE_H
 #define OUTPUTBUNDLE_H
 
+#include "set.h"
+
+#include <headers/SerialPort.hpp>
+
+
 
 class outputBundle
 {
 public:
     outputBundle();
+    bool isOutputInBundle(int id);
+private:
+    set set;
+    SerialPort *arduino;
+    QList<Output*> outputsInSet;
 };
 
 #endif // OUTPUTBUNDLE_H
