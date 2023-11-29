b62079ecfc5f6edef6b88506ac77c341684ff171
bitsanddroids <info@bitsanddroids.com>
2023-11-29 21:15:56 +0100

    Added thread sleep when initializing ComBundle for due boards
    Code has been adjusted to include a pause when initializing the ComBundle for due boards. This pause of 3 seconds is needed to allow these boards enough time to reset, ensuring proper operation. This change only affects the creation sequence of due board's ComBundle, not affecting other functionalities.

--- dashboard/models/ComBundle.cpp
+++ dashboard/models/ComBundle.cpp
@@ -3,14 +3,25 @@
 //
 
 #include "ComBundle.h"
-bool ComBundle::isOutputInBundle(int id) {
+
+#include <thread>
+
+bool ComBundle::isOutputInBundle(int id)
+{
     return this->outputs.contains(id);
 }
 
-ComBundle::ComBundle(QString port) {
+ComBundle::ComBundle(QString port)
+{
     this->port = new SerialPort(port.toStdString().c_str());
+    if (this->port->getPortName().find("due") != std::string::npos)
+    {
+        //sleep for x seconds to allow for due boards to reset
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+    }
 }
 
-QMap<int, Output *> ComBundle::getOutputs() {
+QMap<int, Output*> ComBundle::getOutputs()
+{
     return outputs;
 }
